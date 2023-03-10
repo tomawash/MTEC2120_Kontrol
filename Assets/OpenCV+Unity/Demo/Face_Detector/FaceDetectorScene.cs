c35ded962d97e12ba935a3e516119864209caf8d
Student <student@V120-ENT-012.local>
2023-03-10 14:46:30 -0500

    Added Sample WebCam Project

---
+++ Assets/OpenCV+Unity/Demo/Face_Detector/FaceDetectorScene.cs
@@ -0,0 +1,149 @@
+﻿namespace OpenCvSharp.Demo
+{
+	using System;
+	using UnityEngine;
+	using System.Collections.Generic;
+	using UnityEngine.UI;
+	using OpenCvSharp;
+
+	public class FaceDetectorScene : WebCamera
+	{
+		public TextAsset faces;
+		public TextAsset eyes;
+		public TextAsset shapes;
+
+		private FaceProcessorLive<WebCamTexture> processor;
+
+		/// <summary>
+		/// Default initializer for MonoBehavior sub-classes
+		/// </summary>
+		protected override void Awake()
+		{
+			base.Awake();
+			base.forceFrontalCamera = true; // we work with frontal cams here, let's force it for macOS s MacBook doesn't state frontal cam correctly
+
+			//shapes = Resources.Load("shape_predictor_68_face_landmarks.dat") as TextAsset;
+			shapes = (TextAsset)Resources.Load("shape_predictor_68_face_landmarks", typeof(TextAsset));
+
+			Debug.Log("Awake. 1");
+			// Why is shapes null???
+			if (shapes == null) return; 
+			byte[] shapeDat = shapes.bytes;
+
+
+
+
+			if (shapeDat.Length == 0)
+			{
+				string errorMessage =
+					"In order to have Face Landmarks working you must download special pre-trained shape predictor " +
+					"available for free via DLib library website and replace a placeholder file located at " +
+					"\"OpenCV+Unity/Assets/Resources/shape_predictor_68_face_landmarks.bytes\"\n\n" +
+					"Without shape predictor demo will only detect face rects.";
+
+#if UNITY_EDITOR
+				// query user to download the proper shape predictor
+				if (UnityEditor.EditorUtility.DisplayDialog("Shape predictor data missing", errorMessage, "Download", "OK, process with face rects only"))
+					Application.OpenURL("http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2");
+#else
+             UnityEngine.Debug.Log(errorMessage);
+#endif
+			}
+
+			processor = new FaceProcessorLive<WebCamTexture>();
+			processor.Initialize(faces.text, eyes.text, shapes.bytes);
+
+			// data stabilizer - affects face rects, face landmarks etc.
+			processor.DataStabilizer.Enabled = true;        // enable stabilizer
+			processor.DataStabilizer.Threshold = 2.0;       // threshold value in pixels
+			processor.DataStabilizer.SamplesCount = 2;      // how many samples do we need to compute stable data
+
+			// performance data - some tricks to make it work faster
+			processor.Performance.Downscale = 256;          // processed image is pre-scaled down to N px by long side
+			processor.Performance.SkipRate = 0;             // we actually process only each Nth frame (and every frame for skipRate = 0)
+			if (processor == null) Debug.LogError("processor is null!");
+
+			Debug.Log("Awake. 2");
+
+			//processor.
+			Debug.Log("Processor is Initialized. " + processor.ToString());
+
+		}
+
+
+		protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
+		{
+			//Mat img = Unity.TextureToMat(input, TextureParameters);
+
+			////Convert image to grayscale
+			//Mat imgGray = new Mat();
+			//Cv2.CvtColor(img, imgGray, ColorConversionCodes.BGR2GRAY);
+
+			//// Clean up image using Gaussian Blur
+			//Mat imgGrayBlur = new Mat();
+			//Cv2.GaussianBlur(imgGray, imgGrayBlur, new Size(5, 5), 0);
+
+			////Extract edges
+			//Mat cannyEdges = new Mat();
+			//Cv2.Canny(imgGrayBlur, cannyEdges, 10.0, 70.0);
+
+			////Do an invert binarize the image
+			//Mat mask = new Mat();
+			//Cv2.Threshold(cannyEdges, mask, 70.0, 255.0, ThresholdTypes.BinaryInv);
+
+			//// result, passing output texture as parameter allows to re-use it's buffer
+			//// should output texture be null a new texture will be created
+			//output = Unity.MatToTexture(mask, output);
+			//return true;
+
+
+			if (processor == null)
+			{
+				return false;
+			}
+            //	// detect everything we're interested in
+
+            if (input == null)
+			{
+				Debug.LogError("Input is null"); 
+				return false;
+			}
+
+			if (TextureParameters == null)
+			{
+				Debug.LogError("TextureParameters is null");
+				return false;
+			}
+
+			processor.ProcessTexture(input, TextureParameters);
+
+            // mark detected objects
+            processor.MarkDetected();
+
+            // processor.Image now holds data we'd like to visualize
+            output = Unity.MatToTexture(processor.Image, output);   // if output is valid texture it's buffer will be re-used, otherwise it will be re-created
+
+            return true;
+
+
+        }
+
+
+		/// <summary>
+		/// Per-frame video capture processor
+		/// </summary>
+		//protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
+		//{
+		//	// detect everything we're interested in
+		//	processor.ProcessTexture(input, TextureParameters);
+
+		//	// mark detected objects
+		//	processor.MarkDetected();
+
+		//	// processor.Image now holds data we'd like to visualize
+		//	output = Unity.MatToTexture(processor.Image, output);   // if output is valid texture it's buffer will be re-used, otherwise it will be re-created
+
+		//	return true;
+		//}
+	}
+}
---
+++ Assets/Script/PlayerTrackedObject.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+using OpenCvSharp.Demo;
+public class PlayerTrackedObject : MonoBehaviour
+{
+
+	public TrackingScript trackedObj;
+	public RawImage rawImage;
+	public float scaleX;
+	public float scaleY;
+
+	public float offSetX;
+	public float offSetY; 
+
+	bool dead;
+	public AudioClip[] auClip;
+	public GameObject fire;
+
+	float lastY = 0; 
+
+	void Start()
+	{
+		dead = false;
+		GetComponent<AudioSource>().clip = auClip[0];
+	}
+
+	void Update()
+	{
+
+		Vector2 pos2D = trackedObj.GetImageCoord();
+		
+		Vector2 output = new Vector2();
+		RectTransformUtility.ScreenPointToLocalPointInRectangle(rawImage.rectTransform, pos2D, Camera.main, out output);
+
+
+		//float x = output.x*scaleX - offSetX;
+		//float y = output.y*scaleY - offSetY;
+		//Debug.Log("Player Update : " + x + " " + y);
+
+
+		float norm = 3*Mathf.Clamp(output.y - lastY, -1, 1);
+		//transform.position = new Vector3(x, y, 0) ;
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y - norm, transform.position.z), 1.0f);
+		lastY = output.y; 
+
+
+		//if (Input. GetMouseButtonDown(0) && !dead)
+		//{
+		//	RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+		//	if (hit.collider == null)
+		//	{
+		//		Jump();
+		//	}
+		//}
+	}
+
+	//void Jump()
+	//{
+	//	fire.SetActive(true);
+	//	GetComponent<AudioSource>().Play();
+	//	GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+	//	GetComponent<Rigidbody2D>().AddForce(Vector2.up * 200);
+	//}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (!dead)
+		{
+			if (col.tag == "Score")
+			{
+				GameObject.FindObjectOfType<GameManager>().Score++;
+				Destroy(col.gameObject);
+			}
+			else if (col.tag == "Finish")
+			{
+				//dead = true;
+				GameObject.FindObjectOfType<GameManager>().Score--;
+				GetComponent<AudioSource>().clip = auClip[1];
+				GetComponent<AudioSource>().Play();
+				Invoke("BackToMain", 1.5f);
+			}
+		}
+	}
+
+	void BackToMain()
+	{
+
+        trackedObj.Disable();
+
+        SceneManager.LoadScene("MainMenu");
+	}
+}
